6188c037593a56ee87bb19d966feff2fe501b1da
Justin Phelps <linuturk@onitato.com>
2015-02-25 17:50:22 -0600

    cleaning up spacing and adding the apt-get command for git

--- localsalt.sh
+++ localsalt.sh
@@ -2,21 +2,17 @@
 
 # Install salt-minion using Salt Bootstrap
 curl -L https://bootstrap.saltstack.com | sudo sh -s --
-
 # Set salt-minion to only use local resources
 echo "file_client: local" > /etc/salt/minion.d/local.conf
-
 # Make Pillar folder
 mkdir /srv/pillar
-
 # Clone down States
+apt-get install git -y
 git clone https://github.com/rackspace-orchestration-templates/salt-states.git /srv/salt
-
 # Write out Pillar top.sls
 echo "base:
   '*':
     - localhost" > /srv/pillar/top.sls
-
 # Write out State top.sls
 echo "base:
   '*':
@@ -25,13 +21,11 @@ echo "base:
 #######################################
 ## Edit this section ##################
 #######################################
-
 # Add states to the base (4 spaces)
 echo "    - state1
     - state2
     - state3
 " >> /srv/salt/top.sls
-
 # Example Pillar Data using %value% notation
 # See example pillar data from states repository.
 echo "
@@ -43,13 +37,9 @@ key2:
 key3:
   nested_key: %nested_value%
 " > /srv/pillar/localhost.sls
-
 #######################################
 ## END ################################
 #######################################
 
 # Run local highstate
 salt-call --local state.highstate
-
-# Run a 2nd time and check for changes TODO Find a better way
-#salt-call -l critical --local state.highstate | grep changed
